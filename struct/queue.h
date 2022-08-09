17232b8a4690d84d7e91e4f9ccfc942abf89920c
Entropov <81511837+Entropov@users.noreply.github.com>
2022-08-09 22:15:25 +0800

    Update queue.h

--- struct/queue.h
+++ struct/queue.h
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#ifndef BOOL_H
+#define BOOL_H
+typedef enum{true = 1,false = 0}bool;//cpp关键字报错
+#endif
 #define MAX_SIZE 100//循环队列，最大容量99
 typedef struct queue
 {
@@ -7,8 +11,6 @@ typedef struct queue
     int h;
     int r;
 }queue;
-
-typedef enum{true = 1,false = 0}bool;//cpp关键字报错
 
 queue* init_queue()
 {
@@ -21,19 +23,19 @@ queue* init_queue()
 } 
 
 /*返回队头元素*/
-int front(queue* queue)
+int frontOfQueue(queue* queue)
 {
     return queue->q[queue->h];
 }
 
 /*返回队尾元素*/
-int back(queue* queue)
+int backOfQueue(queue* queue)
 {
     return queue->q[queue->r-1];
 }
 
 /*判空*/
-bool empty(queue* queue)
+bool queue_empty(queue* queue)
 {
     if(queue->h == queue->r){
         return true;
@@ -48,7 +50,7 @@ int queue_size(queue* queue)
 }
 
 /*队尾入队*/
-void push(queue* queue, int x)
+void pushQueue(queue* queue, int x)
 {
     if(queue_size(queue) == MAX_SIZE-1){
         printf("full queue!");
@@ -60,9 +62,9 @@ void push(queue* queue, int x)
 }
 
 /*队头出队*/
-void pop(queue* queue)
+void popQueue(queue* queue)
 {
-    if(empty(queue)){
+    if(queue_empty(queue)){
         printf("empty queue!");
         return;
     }else{
