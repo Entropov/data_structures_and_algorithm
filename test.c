ca9dd19b084fc320b7dba037540beb2706345d73
Entropov <81511837+Entropov@users.noreply.github.com>
2022-08-10 21:07:32 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "struct\adjList.h"
-#include "struct\binaryTree.h"
-#include "struct\queue.h"
-#include "struct\stack.h"
-#include "struct\linkList.h"
+#include "struct/adjList.h"
+#include "struct/binaryTree.h"
+#include "struct/queue.h"
+#include "struct/stack.h"
+#include "struct/linkList.h"
 
 void testQueue();
 void testStack();
@@ -53,14 +53,14 @@ int main()
         }
         printf("\n");
         system("pause");
-            printf("This is a test:\n");
-            printf("    press 1 to test Queue.\n");
-            printf("    press 2 to test Stack.\n");
-            printf("    press 3 to test LinkList.\n");
-            printf("    press 4 to test BinaryTree.\n");
-            printf("    press 5 to test BinaryTree2.\n");
-            printf("    press 6 to test AdjList.\n");
-            printf("    press 0 to quit.\n");
+        printf("This is a test:\n");
+        printf("    press 1 to test Queue.\n");
+        printf("    press 2 to test Stack.\n");
+        printf("    press 3 to test LinkList.\n");
+        printf("    press 4 to test BinaryTree.\n");
+        printf("    press 5 to test BinaryTree2.\n");
+        printf("    press 6 to test AdjList.\n");
+        printf("    press 0 to quit.\n");
         scanf("%d",&a);
     }
     return 0;
@@ -69,11 +69,35 @@ int main()
 void testQueue()
 {
     queue* q = init_queue();
+    for(int i = 1; i <= 10; i++){
+        pushQueue(q,i);
+    }
+    printf("The size is %d\n",queue_size(q));
+    printf("front: %4d\n",frontOfQueue(q));
+    printf("back: %4d\n",backOfQueue(q));
+    printf("pop 5 elements...\n");
+    for(int k = 0; k < 5; k++){
+        popQueue(q);
+    }
+    printf("The size is %d\n",queue_size(q));
+    printf("front: %4d\n",frontOfQueue(q));
+    printf("back: %4d\n",backOfQueue(q));
 }
 
 void testStack()
 {
     stack* s = init_stack();
+    for(int i = 1; i <= 10; i++){
+        pushStack(s,i);
+    }
+    printf("The size is %d\n",stack_size(s));
+    printf("top: %4d\n",topOfStack(s));
+    printf("pop 3 elements...\n");
+    for(int k = 0; k < 3; k++){
+        popStack(s);
+    }
+    printf("The size is %d\n",stack_size(s));
+    printf("top: %4d\n",topOfStack(s));
 }
 
 void testLinkList()
@@ -83,6 +107,22 @@ void testLinkList()
     printf("plz input size:\n");
     scanf("%d",&size);
     creatLink(list,size);
+    printf("Link list is ok.\n");
+    showList(list);
+    int ak,al;
+    printf("plz input the location to reset:\n");
+    scanf("%d",&ak);
+    printf("plz input the data to reset:\n");
+    scanf("%d",&al);
+    setNode(list,al,ak);
+    printf("Now the size is: %d\n",link_size(list));
+    showList(list);
+    int jk;
+    printf("plz input the location to delete:\n");
+    scanf("%d",&jk);
+    deleteNode(list,jk);
+    printf("Now the size is: %d\n",link_size(list));
+    showList(list);
 }
 
 void testBinaryTree()
@@ -92,10 +132,11 @@ void testBinaryTree()
     for(int i=0;i<7;i++){
         addLeaf(root,testList[i]);
     }
+    printf("pre  order:");
     preOrder(root);
-    printf("\n");
+    printf("\nin   order:");
     inOrder(root);
-    printf("\n");
+    printf("\npost order:");
     postOrder(root);
     printf("\n plz input data to delete:\n");
     int aq;
@@ -112,11 +153,12 @@ void testBinaryTree2()
     for(int i=0;i<14;i++){
         addLeaf(root,a[i]);
     }
-    printf("%d\n",getDepthOfBinary(root));
+    printf("The depth is %d\n",getDepthOfBinary(root));
+    printf("pre  order:");
     preOrder(root);
-    printf("\n");
+    printf("\nin   order:");
     inOrder(root);
-    printf("\n");
+    printf("\npost order:");
     postOrder(root);
     printf("\n plz input data to delete:\n");
     int aq;
@@ -124,7 +166,7 @@ void testBinaryTree2()
     deleteBinaryNode(root,aq);
     inOrder(root);
     printf("\n");
-    printf("%d\n",getDepthOfBinary(root));
+    printf("The depth is %d\n",getDepthOfBinary(root));
 }
 
 void testAdjList()
